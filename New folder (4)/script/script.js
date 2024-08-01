458e5e16bf68c918c9d3086a9ff8163e3144f4a3
fahimeh <fahimeh.zandevakili@gmail.com>
2024-08-01 17:17:37 +0330

    Add files via upload

---
+++ New folder (4)/script/script.js
@@ -0,0 +1,6 @@
+const fullName= "fahimeh zandevakili";
+const b= (fullName.indexOf(" "));
+const firstName= fullName.slice(0,b);
+console.log(firstName);
+const lastName= fullName.slice(b+1,);
+console.log(lastName);
