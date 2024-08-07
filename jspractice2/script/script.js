351ade58658c0be2880f08c70bad881d68980480
fahimeh zandevakili <fahimeh.zandevakili@gmail.com>
2024-08-07 10:13:24 +0330

    Add files via upload

---
+++ jspractice2/script/script.js
@@ -0,0 +1,29 @@
+////////// one //////////
+const sports= ["Gymnastics", "Parkour", "Walking" ,"Archery", "Badminton"];
+// console.log(sports);
+
+
+////////// two //////////
+// sports.unshift("i love");
+// console.log(sports);
+
+
+////////// three //////////
+// console.log(sports.join(" and "));
+
+
+////////// four //////////
+// sports.pop();
+// console.log(sports);
+
+
+////////// five //////////
+// const newList= sports.slice();
+// newList.splice(0,2);
+// console.log(newList);
+
+////////// six //////////
+// const [firstItem, ]= sports;
+// const [ ,...items]= sports;
+// console.log(firstItem);
+// console.log(items);
